c12981802f01c59097fc0cefc88788265be9c171
matthewkingston <matthew.kingston@aquaq.co.uk>
2020-02-26 14:11:30 +0000

    Added commenting to show user editable parts

--- TastyMessage/login.sh
+++ TastyMessage/login.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
+#edit this line to add IP address of hub (and port if not default)
 connection=[IP address of TastyHub]:4242
 read -p 'Username: ' user
 read -sp 'Password: ' pass
+#if cannot call q from inside a script, below replace 'q' with path to your q exectuable
 rlwrap -r q TastyUser.q $connection $user $pass
